884150718fb4232648788346aa1e0071fecb1555
mehmet <beyzadetoprak@gmail.com>
2022-08-21 13:32:33 +0200

    added coinbase api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import App from "./App";
+import "antd/dist/antd.css";
+import store from "./app/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- 
-    <App />
-  
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
 );
-
-
